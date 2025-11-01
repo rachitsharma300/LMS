ee64e02cddcf4bc17c025963dbf46f4b672986d1
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:21:34 +0530

    feat: AuthService

--- lms.backend/src/main/java/com/lms/backend/service/AuthService.java
+++ lms.backend/src/main/java/com/lms/backend/service/AuthService.java
@@ -1,12 +1,11 @@
 package com.lms.backend.service;
 
-import com.lms.backend.dto.LoginRequest;
 import com.lms.backend.dto.SignupRequest;
 import com.lms.backend.model.User;
-import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
-    ResponseEntity<?> registerUser(SignupRequest signupRequest);
-    ResponseEntity<?> loginUser(LoginRequest loginRequest);
+    User registerUser(SignupRequest signupRequest);
+
+    // Ye line add karo agar nahi hai 👇
     User getCurrentUser();
 }
