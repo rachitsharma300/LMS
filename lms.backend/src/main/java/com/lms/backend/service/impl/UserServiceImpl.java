05a9162658e45b5e42d9ea8e012beca4ce7214ef
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:27:12 +0530

    feat: LMS Init package and java class

---
+++ lms.backend/src/main/java/com/lms/backend/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.lms.backend.service.impl;
+
+import com.lms.backend.exception.ResourceNotFoundException;
+import com.lms.backend.model.User;
+import com.lms.backend.repository.UserRepository;
+import com.lms.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with id: " + id));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User existingUser = getUserById(id);
+        userRepository.delete(existingUser);
+    }
+}
