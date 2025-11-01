b8352089f13af580d6e2ea6b406a6581ba135bce
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:24:46 +0530

    feat: LoginRequest

--- lms.backend/src/main/java/com/lms/backend/dto/LoginRequest.java
+++ lms.backend/src/main/java/com/lms/backend/dto/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.lms.backend.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+    private String email;
+    private String password;
 }
