e6f75545f97dcf9d84afd508f0c38c858edeb893
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:26:15 +0530

    feat: LMS Init package and java class

--- lms.backend/src/main/java/com/lms/backend/repository/UserRepository.java
+++ lms.backend/src/main/java/com/lms/backend/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.lms.backend.repository;
 
 import com.lms.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
