d171e4c6b86dfed2873f39c7dfa12fe62b8f71a5
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:26:06 +0530

    feat: LMS Init package and java class

--- lms.backend/src/main/java/com/lms/backend/dto/SignupRequest.java
+++ lms.backend/src/main/java/com/lms/backend/dto/SignupRequest.java
@@ -1,4 +1,18 @@
 package com.lms.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SignupRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private String role; // Example: "ROLE_STUDENT", "ROLE_INSTRUCTOR", "ROLE_ADMIN"
 }
