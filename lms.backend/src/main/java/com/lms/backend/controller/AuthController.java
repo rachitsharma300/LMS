74c027159abd180e9c8b4d0e222cd059d5fcb2e3
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:21:10 +0530

    feat: AuthCOntroller

--- lms.backend/src/main/java/com/lms/backend/controller/AuthController.java
+++ lms.backend/src/main/java/com/lms/backend/controller/AuthController.java
@@ -1,4 +1,43 @@
 package com.lms.backend.controller;
 
+import com.lms.backend.dto.LoginRequest;
+import com.lms.backend.dto.SignupRequest;
+import com.lms.backend.model.User;
+import com.lms.backend.security.JwtTokenProvider;
+import com.lms.backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/signup")
+    public User registerUser(@RequestBody SignupRequest signupRequest) {
+        return authService.registerUser(signupRequest);
+    }
+
+    @PostMapping("/login")
+    public String authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+        return jwtTokenProvider.generateToken(authentication);
+    }
 }
