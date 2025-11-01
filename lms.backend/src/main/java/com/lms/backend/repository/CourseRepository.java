8095c69176892d43f5a9c518fe644f40673f9003
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:22:27 +0530

    feat: CourseRepo

--- lms.backend/src/main/java/com/lms/backend/repository/CourseRepository.java
+++ lms.backend/src/main/java/com/lms/backend/repository/CourseRepository.java
@@ -1,12 +1,13 @@
 package com.lms.backend.repository;
 
-
 import com.lms.backend.model.Course;
 import com.lms.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByInstructor(User instructor);
-    List<Course> findByApproved(boolean approved);
 }
