463f25b9acb1fdb755abcd515fc155c990a1be85
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-01 23:25:42 +0530

    feat: LMS Init package and java class

--- lms.backend/src/main/java/com/lms/backend/service/StudentService.java
+++ lms.backend/src/main/java/com/lms/backend/service/StudentService.java
@@ -1,11 +1,10 @@
 package com.lms.backend.service;
 
 import com.lms.backend.model.Course;
-import com.lms.backend.model.Enrollment;
 import com.lms.backend.model.User;
 import java.util.List;
 
 public interface StudentService {
-    Enrollment enrollInCourse(User student, Course course);
+    void enrollCourse(User student, Long courseId);
     List<Course> getEnrolledCourses(User student);
 }
