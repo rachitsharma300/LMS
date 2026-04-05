293e08bc4dc4f056b45336b1bc1ac9b3c5ab7535
rachitsharma300 <rachitsharma300@gmail.com>
2026-04-05 10:01:51 +0530

    Update API base URL to use production endpoint on Render

--- lms.frontend/src/services/apiClient.js
+++ lms.frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://lms-80z3.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
